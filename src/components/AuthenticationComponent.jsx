9a3f40157d5e5c964236018018f534b479c4dacb
anzhelikakuchma <anzhelika.kuchma@gmail.com>
2024-08-04 21:54:26 -0230

    added form component

--- src/components/AuthenticationComponent.jsx
+++ src/components/AuthenticationComponent.jsx
@@ -1,10 +1,7 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
 import MainContext from "../context/MainContext.jsx";
-
-import FormComponent from "./FormComponent";
-
+import CredentialsFormComponent from "./CredentialsFormComponent";
 import {
     baseURI,
     procedurePath,
@@ -14,44 +11,36 @@ import {
 
 import { submitFormData } from "../api/serverAPI.js";
 
-const AuthenticationComponent = ({ pathname, title }) => {
+const AuthenticationComponent = ({ pathname }) => {
     const { setIsAuthenticated } = useContext(MainContext);
     const navigate = useNavigate();
 
     const url = baseURI + pathname;
-
     const handleRedirect = () => {
         switch (pathname) {
             case loginPath:
                 navigate(procedurePath);
                 break;
-
             case registrationPath:
                 navigate(loginPath);
                 break;
         }
     };
-
     const handleOnSubmit = (event) => {
         event.preventDefault();
-
         const { username, password } = event.target;
         const formDataObj = {
             name: username.value,
             password: password.value,
         };
-
         const statusCode = submitFormData(url, formDataObj);
-
         if (statusCode == 200) {
             pathname === loginPath && setIsAuthenticated(true);
-
             handleRedirect(pathname);
         }
     };
 
-    return (
-        <FormComponent onSubmit={handleOnSubmit} title={title} action={url} />
-    );
+    return <CredentialsFormComponent onSubmit={handleOnSubmit} action={url} />;
 };
+
 export default AuthenticationComponent;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,10 @@
 import { useLocation } from "react-router-dom";
-
 import AuthenticationComponent from "../components/AuthenticationComponent.jsx";
 
 const LoginPage = () => {
     const { pathname } = useLocation();
-    const title = "login";
 
-    return <AuthenticationComponent pathname={pathname} title={title} />;
+    return <AuthenticationComponent pathname={pathname} />;
 };
 
 export default LoginPage;
